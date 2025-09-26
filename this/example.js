0dd6adb0e81496c530a2e4c5a87b41e6c1fc7979
kharabeaniruddha <kharabeaniruddha@gmail.com>
2025-09-26 08:54:39 +0530

    this object

---
+++ this/example.js
@@ -0,0 +1,66 @@
+// "use strict";
+var globalVariable = "this is inside global scope";
+// window object
+console.log("window", window);
+console.log("this", this); // global scope
+console.log(globalVariable);
+console.log(window.globalVariable);
+console.log(this.globalVariable);
+
+// this is inside of function
+function insideFunction() {
+  console.log("inside function", this);
+}
+
+insideFunction();
+
+// this inside of object
+
+const user = {
+  name: "Karn",
+  getName: function () {
+    console.log("inside object with function", this);
+    console.log("hey " + this.name);
+  },
+};
+
+user.getName();
+
+const user2 = {
+  name: "Arjun",
+  getName: () => {
+    console.log("inside object with arrow function", this); //surrounding lexical scope
+  },
+};
+
+user2.getName();
+
+// this inside class
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+  getName() {
+    console.log("hello " + this.name);
+  }
+}
+
+const p1 = new Person("Sunil");
+p1.getName();
+
+// call/apply/bind
+
+const student = {
+  name: "Karn",
+  getName: function () {
+    console.log("hey " + this.name);
+  },
+};
+
+const sudent2 = {
+  name: "Arjun",
+};
+student.getName();
+
+student.getName.call(sudent2);
